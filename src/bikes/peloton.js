2f392a9244c4fbdb2531d9dc2f7f77f0ff707a92
Jeremy Klein <jeremydk@gmail.com>
2021-02-10 18:53:05 -0800

    Add tracelog support for Peloton

--- src/bikes/peloton.js
+++ src/bikes/peloton.js
@@ -1,6 +1,7 @@
 import {once, EventEmitter} from 'events';
 import {Timer} from '../util/timer';
 import util from 'util';
+import { trace } from 'console';
 
 const SerialPort = require('serialport')
 const Delimiter = require('@serialport/parser-delimiter')
@@ -9,6 +10,7 @@ const PACKET_DELIMITER = Buffer.from('f6', 'hex');
 const STATS_TIMEOUT = 1.0;
 
 const debuglog = util.debuglog('gymnasticon:bikes:peloton');
+const tracelog = util.debuglog('gymnasticon:bikes:peloton:trace');
 
 export class PelotonBikeClient extends EventEmitter {
   /**
@@ -40,11 +42,13 @@ export class PelotonBikeClient extends EventEmitter {
     this._port = new SerialPort(this.path, {baudRate: 19200, autoOpen: false});
     const open = util.promisify(this._port.open.bind(this._port));
     await open();
+    tracelog("Serial Opened")
     this._port.on('close', this.onSerialClose);
     this._parser = this._port.pipe(new Delimiter({ delimiter: PACKET_DELIMITER }));
     this._parser.on('data', this.onSerialMessage);
 
     this.state = 'connected';
+    tracelog("Serial Connected");
   }
 
   /**
@@ -64,27 +68,33 @@ export class PelotonBikeClient extends EventEmitter {
   }
 
   onSerialMessage(data) {
+    tracelog("RECV: ", data);
     switch(data[1]) {
-    case 65:
-      this.cadence = decodePeloton(data, data[2], false);
-      this.onStatsUpdate();
-      this.statsTimeout.reset();
-      return;
-    case 68:
-      this.power = decodePeloton(data, data[2], true);
-      this.onStatsUpdate();
-      this.statsTimeout.reset();
-      return;
-    }
+      case 65:
+        this.cadence = decodePeloton(data, data[2], false);
+        this.onStatsUpdate();
+        this.statsTimeout.reset();
+        return;
+      case 68:
+        this.power = decodePeloton(data, data[2], true);
+        this.onStatsUpdate();
+        this.statsTimeout.reset();
+        return;
+      default:
+        debuglog("Unrecognized Message Type: ", data[1]);
+        return;
+      }
   }
 
   onSerialClose() {
     this.emit('disconnect', {address: this.address});
+    tracelog("Serial Closed");
   }
 
   onStatsTimeout() {
     this.power = 0;
     this.cadence = 0;
+    tracelog("StatsTimeout exceeded");
     this.onStatsUpdate();
   }
 }
