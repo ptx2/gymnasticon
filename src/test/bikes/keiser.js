b3fe3e6c47e13905940638b08f2bd35f4f955333
Christian Elsen <chriselsen@users.noreply.github.com>
2021-02-17 11:31:07 -0800

    Updated keiser test

--- src/test/bikes/keiser.js
+++ src/test/bikes/keiser.js
@@ -3,7 +3,8 @@ import {parse} from '../../bikes/keiser';
 
 test('parse() parses Keiser indoor bike data values', t => {
   const buf = Buffer.from('0201063000383803460573000D00042701000A', 'hex');
-  const {power, cadence} = parse(buf);
+  const {type, payload: {power, cadence}} = parse(buf);
+  t.equal(type, 'stats', 'message type');
   t.equal(power, 115, 'power (watts)');
   t.equal(cadence, 82, 'cadence (rpm)');
   t.end();
