c6598135a4b6822eb6b3bc7d06f7273a92d25d1d
Christian Elsen <chriselsen@users.noreply.github.com>
2021-02-18 09:46:55 -0800

    Better documentation of Keiser test case

--- src/test/bikes/keiser.js
+++ src/test/bikes/keiser.js
@@ -1,6 +1,10 @@
 import test from 'tape';
 import {parse} from '../../bikes/keiser';
 
+/**
+ * See https://dev.keiser.com/mseries/direct/#data-parse-example for a
+ * data parse example of the below test case
+ */
 test('parse() parses Keiser indoor bike data values', t => {
   const buf = Buffer.from('0201063000383803460573000D00042701000A', 'hex');
   const {type, payload: {power, cadence}} = parse(buf);
