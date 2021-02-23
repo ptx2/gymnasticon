7b216daf8438ba7171deaa774278356d836bbdb8
ptx2 <ptx2@ptx2.net>
2021-02-22 19:27:38 -0500

    Add some comments.

--- src/util/logger.js
+++ src/util/logger.js
@@ -8,6 +8,18 @@ const defaults = {
  * Timestamped logger.
  */
 export class Logger {
+  /**
+   * Create a Logger instance.
+   *
+   * For each log level, a method of the same name is added to the instance.
+   * The method calls logFunction if its level is greater than or equal to
+   * the Logger's level. Otherwise it calls an empty no-op function.
+   *
+   * @param {object} options
+   * @param {function} options.logFunction - defaults to console.log
+   * @param {string} options.level - log level (msgs below this level are not logged)
+   * @param {string[]} options.levels - log levels
+   */
   constructor(options = {}) {
     const {logFunction, level, levels} = {...defaults, ...options};
 
@@ -19,6 +31,7 @@ export class Logger {
     const log = (...args) => logFunction(this.prefix, ...args);
     const doNotLog = () => {};
 
+    // Attach the appropriate method for each log level
     levels.forEach((l, i) => {
       this[l] = levelIndex <= i ? log : doNotLog;
     });
