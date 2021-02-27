3974bbd06096c088eb6fda6d0952ba06f1a979be
Christian Elsen <chriselsen@users.noreply.github.com>
2021-02-27 15:41:14 -0800

    Upon boot lock /boot filesystem with RO if / is overlayfs in RO mode

--- deploy/pi-sdcard/stage-gymnasticon/00-install-gymnasticon/01-run.sh
+++ deploy/pi-sdcard/stage-gymnasticon/00-install-gymnasticon/01-run.sh
@@ -29,11 +29,14 @@ EOF
 install -v -m 644 files/gymnasticon.json "${ROOTFS_DIR}/etc/gymnasticon.json"
 install -v -m 644 files/gymnasticon.service "${ROOTFS_DIR}/etc/systemd/system/gymnasticon.service"
 install -v -m 644 files/gymnasticon-mods.service "${ROOTFS_DIR}/etc/systemd/system/gymnasticon-mods.service"
+install -v -m 644 files/gymnasticon-mods.service "${ROOTFS_DIR}/etc/systemd/system/lockbootfs.service"
 install -v -m 755 files/overctl "${ROOTFS_DIR}/usr/local/sbin/overctl"
+install -v -m 755 files/lockbootfs "${ROOTFS_DIR}/usr/local/sbin/lockbootfs"
 
 on_chroot <<EOF
 systemctl enable gymnasticon
 systemctl enable gymnasticon-mods
+systemctl enable lockbootfs
 
 dphys-swapfile swapoff
 dphys-swapfile uninstall
