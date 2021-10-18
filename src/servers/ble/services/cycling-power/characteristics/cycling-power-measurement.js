f686eaba49099e10285296906019623878f670b8
Christian Elsen <chriselsen@users.noreply.github.com>
2021-10-18 08:29:36 -0700

    Support for Wheel Revolution Data in BLE Cycling Power

--- src/servers/ble/services/cycling-power/characteristics/cycling-power-measurement.js
+++ src/servers/ble/services/cycling-power/characteristics/cycling-power-measurement.js
@@ -3,8 +3,9 @@ import {Characteristic, Descriptor} from '@abandonware/bleno';
 const debuglog = require('debug')('gym:servers:ble');
 
 const FLAG_HASCRANKDATA = (1<<5);
+const FLAG_HASSPEEDDATA = (1<<4);
 const CRANK_TIMESTAMP_SCALE = 1024 / 1000; // timestamp resolution is 1/1024 sec
-
+const WHEEL_TIMESTAMP_SCALE = 1024 / 1000; // timestamp resolution is 1/1024 sec
 
 /**
  * Bluetooth LE GATT Cycling Power Measurement Characteristic implementation.
@@ -27,22 +28,31 @@ export class CyclingPowerMeasurementCharacteristic extends Characteristic {
    * Notify subscriber (e.g. Zwift) of new Cycling Power Measurement.
    * @param {object} measurement - new cycling power measurement.
    * @param {number} measurement.power - current power (watts)
-	 * @param {object} [measurement.crank] - last crank event.
+	 * @param {object} measurement.crank - last crank event.
    * @param {number} measurement.crank.revolutions - revolution count at last crank event.
    * @param {number} measurement.crank.timestamp - timestamp at last crank event.
+   * @param {object} measurement.wheel - last wheel event.
+   * @param {number} measurement.wheel.revolutions - revolution count at last wheel event.
+   * @param {number} measurement.wheel.timestamp - timestamp at last wheel event.
    */
-  updateMeasurement({ power, crank }) {
+   */
+  updateMeasurement({ power, crank, wheel }) {
     let flags = 0;
 
-    const value = Buffer.alloc(8);
+    const value = Buffer.alloc(14);
     value.writeInt16LE(power, 2);
 
-    if (crank) {
-      const revolutions16bit = crank.revolutions & 0xffff;
-      const timestamp16bit = Math.round(crank.timestamp * CRANK_TIMESTAMP_SCALE) & 0xffff;
-      value.writeUInt16LE(revolutions16bit, 4);
-      value.writeUInt16LE(timestamp16bit, 6);
+    if (crank && wheel) {
+      const wheelRevolutions32bit = wheel.revolutions & 0xffffffff;
+      const wheelTimestamp16bit = Math.round(wheel.timestamp * WHEEL_TIMESTAMP_SCALE) & 0xffff;
+      value.writeUInt32LE(wheelRevolutions32bit, 4);
+      value.writeUInt16LE(wheelTimestamp16bit, 8);
+      const crankRevolutions16bit = crank.revolutions & 0xffff;
+      const crankTimestamp16bit = Math.round(crank.timestamp * CRANK_TIMESTAMP_SCALE) & 0xffff;
+      value.writeUInt16LE(crankRevolutions16bit, 10);
+      value.writeUInt16LE(crankTimestamp16bit, 12);
       flags |= FLAG_HASCRANKDATA;
+      flags |= FLAG_HASSPEEDDATA;
     }
 
     value.writeUInt16LE(flags, 0);
